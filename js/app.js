fa16d39b847963aefafce9f0031e4acc0304ae50
anuoluwapo <anuoluwapokudirat@yahoo.com>
2020-03-13 20:54:57 +0000

    fixed the layout and nav-bar

---
+++ js/app.js
@@ -0,0 +1,11 @@
+// let header = document.getElementById("header");
+// window.onscroll = function(){
+//     if(window.pageYOffset > 100){
+//         header.style.position = 'fixed';
+//         header.style.top = 0;
+//     }
+//     else{
+//         header.style.position = 'absolute';
+//         header.style.top = 100;
+//     }
+// }
